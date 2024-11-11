6023c950538bc144e4afe2d983623dbfb4f3bb8e
j4n-r <jan.ruegge@gmail.com>
2024-11-11 15:32:21 +0100

    start protocol impl

--- capture/pkg/capture/capture.go
+++ capture/pkg/capture/capture.go
@@ -1,8 +1,10 @@
 package capture
 
 import (
+	"encoding/binary"
 	"fmt"
 	"log"
+	"net"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -11,13 +13,28 @@ import (
 )
 
 type Packet struct {
-	src_addr string
-	src_port string
-	dst_addr string
-	dst_port string
+	version   byte
+	protocol  layers.ProtocolFamily // byte
+	timestamp uint32
+	src_addr  net.IP
+	src_port  net.IP
+	dst_addr  []byte
+	dst_port  uint16
+}
+
+func constructPayload(buf []byte, p Packet) ([]byte, error) {
+	p.version = 0x01
+    
+    if p.src_addr.To4() != nil {
+
+    }
+    
+	binary.Append(buf, binary.BigEndian, p.version)
+	return buf, nil
 }
 
 func StartCapture(device string, s *websocket.Server) {
+
 	// Open a live capture handle on the "tailscale0" interface
 	handle, err := pcap.OpenLive(device, 65536, true, pcap.BlockForever)
 	if err != nil {
@@ -38,19 +55,19 @@ func handlePacket(packetData []byte, s *websocket.Server) {
 
 	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv4)
-		p.src_addr = ip.SrcIP.String()
-		p.dst_addr = ip.DstIP.String()
+		p.src_addr = ip.SrcIP
+		p.dst_addr = ip.DstIP
 	} else if ipLayer := packet.Layer(layers.LayerTypeIPv6); ipLayer != nil {
 		ip, _ := ipLayer.(*layers.IPv6)
-		p.src_addr = ip.SrcIP.String()
-		p.dst_addr = ip.DstIP.String()
+		p.src_addr = ip.SrcIP
+		p.dst_addr = ip.DstIP
 	}
 
 	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 		// Get actual TCP data from this layer and print source/destination ports
 		tcp, _ := tcpLayer.(*layers.TCP)
-		p.src_port = tcp.SrcPort.String()
-		p.dst_port = tcp.DstPort.String()
+		p.src_port = uint16(tcp.SrcPort)
+		p.dst_port = uint16(tcp.DstPort)
 	}
 
 	var layerType gopacket.LayerType
@@ -61,6 +78,8 @@ func handlePacket(packetData []byte, s *websocket.Server) {
 	case layers.LayerTypeDNS:
 	default:
 		fmt.Printf("%s src: %s:%s dst:%s:%s\n", layerType, p.src_addr, p.src_port, p.dst_addr, p.dst_port)
-		s.C.WriteMessage(s.MessageType, []byte(fmt.Sprintf("%s src: %s:%s dst:%s:%s\n", layerType, p.src_addr, p.src_port, p.dst_addr, p.dst_port)))
+		if s.C != nil {
+			s.C.WriteMessage(s.MessageType, []byte(fmt.Sprintf("%s src: %s:%s dst:%s:%s\n", layerType, p.src_addr, p.src_port, p.dst_addr, p.dst_port)))
+		}
 	}
 }
